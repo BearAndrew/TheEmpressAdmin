1ddfede30003a0cf8d2d1860589c17b17ee6e611
BearAndrew <a122333andrew@gmail.com>
2021-12-24 15:38:56 +0800

    firebase messaging sw

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -23,7 +23,14 @@
  const messaging = firebase.messaging();
  // [END initialize_firebase_in_sw]
 
-
+ if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./TheEmpressAdmin/firebase-messaging-sw.js')
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    }).catch(function(err) {
+      console.log('Service worker registration failed, error:', err);
+    });
+  }
 
  //#region notifiction點擊事件
  var click_action;
